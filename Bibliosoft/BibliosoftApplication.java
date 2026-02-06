ddcbdca83a724922d4212305f34dd5e97dbf15e5
Juan <perezorozcojuanesteban@gmail.com>
2026-02-06 10:23:09 -0500

    Se crea Spring boot de bibliosoft

---
+++ Bibliosoft/BibliosoftApplication.java
@@ -0,0 +1,13 @@
+package com.Bibliosoft.Bibliosoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliosoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliosoftApplication.class, args);
+	}
+
+}
